f6370faac91bd9f736cb1e7369358391141518c8
noveenmirza <noveen112@hotmail.com>
2024-03-20 18:32:08 -0400

    first commit

---
+++ .history/backend/index_20240320033604.ts
@@ -0,0 +1,16 @@
+import express from express''';
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ .history/backend/server_20240320065553.js
@@ -0,0 +1,17 @@
+"use strict";
+const express = require('express');
+const cors = require('cors');
+// Create express instance
+const app = express();
+
+//Use CO
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from server!' });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
